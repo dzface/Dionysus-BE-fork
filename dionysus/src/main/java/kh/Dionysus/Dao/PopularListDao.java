0795f9d32753bc7480bb3d41d4e6384d8877528f
Park Sung Jin <lbon321@naver.com>
2024-05-24 18:14:29 +0900

    PopularListDao 수정

--- dionysus/src/main/java/kh/Dionysus/Dao/PopularListDao.java
+++ dionysus/src/main/java/kh/Dionysus/Dao/PopularListDao.java
@@ -9,18 +9,55 @@ import java.util.List;
 
 public class PopularListDao {
     private Connection conn = null;
-    private Statement stmt = null;
     private ResultSet rs = null;
     private PreparedStatement pStmt = null;
     public List<AlcoholTotalDto> popularSelect(String tag) throws SQLException {
         List<AlcoholTotalDto> list = new ArrayList<>();
-        String sql = "SELECT a.ALCOHOL_NAME, a.CATEGORY, a.COUNTRY_OF_ORIGIN, a.COM, a.ABV, a.VOLUME, a.PRICE, a.TAG, " +
-                "j.JJIM, r.REVIEW, s.SCORE " +
-                "FROM ALCOHOL_TB a " +
-                "LEFT JOIN REVIEW_TB r ON a.ALCOHOL_NAME = r.ALCOHOL_NAME " +
-                "LEFT JOIN SCORE_TB s ON a.ALCOHOL_NAME = s.ALCOHOL_NAME " +
-                "LEFT JOIN JJIM_TB j ON a.ALCOHOL_NAME = j.ALCOHOL_NAME " +
-                "WHERE a.TAG LIKE ?";
+        String sql = "WITH RankedReviews AS (\n" +
+                "    SELECT\n" +
+                "        USER_ID,\n" +
+                "        ALCOHOL_NAME,\n" +
+                "        REVIEW,\n" +
+                "        ROW_NUMBER() OVER (PARTITION BY ALCOHOL_NAME ORDER BY USER_ID) AS rn\n" +
+                "    FROM\n" +
+                "        REVIEW_TB\n" +
+                ")\n" +
+                "SELECT *\n" +
+                "FROM (\n" +
+                "    SELECT\n" +
+                "        a.ALCOHOL_NAME,\n" +
+                "        a.COUNTRY_OF_ORIGIN,\n" +
+                "        a.COM,\n" +
+                "        a.ABV,\n" +
+                "        a.VOLUME,\n" +
+                "        a.PRICE,\n" +
+                "        a.TAG,\n" +
+                "        r.REVIEW,\n" +
+                "        s.SCORE,\n" +
+                "        j.USER_ID AS JJIM_USER_ID,\n" +
+                "        j.JJIM,\n" +
+                "        ROW_NUMBER() OVER (ORDER BY COALESCE(s.SCORE, 0) DESC) AS row_num\n" +
+                "    FROM\n" +
+                "        ALCOHOL_TB a\n" +
+                "    LEFT JOIN (\n" +
+                "        SELECT\n" +
+                "            ALCOHOL_NAME,\n" +
+                "            ROUND(AVG(SCORE), 1) AS SCORE\n" +
+                "        FROM\n" +
+                "            SCORE_TB\n" +
+                "        GROUP BY\n" +
+                "            ALCOHOL_NAME\n" +
+                "    ) s ON a.ALCOHOL_NAME = s.ALCOHOL_NAME\n" +
+                "    LEFT JOIN (\n" +
+                "        SELECT\n" +
+                "            USER_ID,\n" +
+                "            ALCOHOL_NAME,\n" +
+                "            JJIM\n" +
+                "        FROM\n" +
+                "            JJIM_TB\n" +
+                "    ) j ON a.ALCOHOL_NAME = j.ALCOHOL_NAME\n" +
+                "    LEFT JOIN RankedReviews r ON a.ALCOHOL_NAME = r.ALCOHOL_NAME AND r.rn = 1\n" +
+                ") WHERE TAG LIKE ?";
 
         try {
             conn = Common.getConnection();
@@ -30,7 +67,6 @@ public class PopularListDao {
             while (rs.next()) {
                 AlcoholTotalDto vo = new AlcoholTotalDto();
                 vo.setAlcohol_name(rs.getString("ALCOHOL_NAME"));
-                vo.setCategory(rs.getString("CATEGORY"));
                 vo.setCountry_of_origin(rs.getString("COUNTRY_OF_ORIGIN"));
                 vo.setCom(rs.getString("COM"));
                 vo.setAbv(rs.getInt("ABV"));
@@ -39,7 +75,7 @@ public class PopularListDao {
                 vo.setTag(rs.getString("TAG"));
                 vo.setJjim(rs.getBoolean("JJIM"));
                 vo.setReview(rs.getString("REVIEW"));
-                vo.setScore(rs.getInt("SCORE"));
+                vo.setScore(rs.getFloat("SCORE"));
                 list.add(vo);
 
 
@@ -48,9 +84,8 @@ public class PopularListDao {
             e.printStackTrace();
         }
         Common.close(rs);
-        Common.close(stmt);
-        Common.close(conn);
         Common.close(pStmt);
+        Common.close(conn);
         return list;
     }
 }
